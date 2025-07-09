72257b6db878ec2cbb36624a04076502750ac90d
Grig <grig@lumeneo.com>
2025-07-09 02:26:09 -0600

    Finalize deployment with working sidebar and navigation

--- build/assets/js/5e95c892.d0053af5.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkcanonical_docs=self.webpackChunkcanonical_docs||[]).push([[9647],{7121:(e,c,s)=>{s.r(c),s.d(c,{default:()=>d});s(6540);var a=s(4164),n=s(5500),r=s(7559),u=s(2831),l=s(1656),o=s(4848);function d(e){return(0,o.jsx)(n.e3,{className:(0,a.A)(r.G.wrapper.docsPages),children:(0,o.jsx)(l.A,{children:(0,u.v)(e.route.routes)})})}}}]);
--- build/assets/js/a7456010.2a9b5366.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkcanonical_docs=self.webpackChunkcanonical_docs||[]).push([[1235],{8552:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
