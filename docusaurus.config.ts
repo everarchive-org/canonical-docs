fcb9037a8e04283d88d8a408fb1c9f2bb964a204
Grig <grig@lumeneo.com>
2025-07-15 20:57:08 -0600

    design

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'EverArchive',
   tagline: 'Infrastructure for Human Memory',
-  favicon: 'favicon.ico',
+  favicon: 'img/favicon/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
